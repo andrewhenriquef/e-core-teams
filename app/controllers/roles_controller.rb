d5048d4c7914f8802ff7389122057ff4d104f73b
Andrew Oliveira <andrewhenriquef@gmail.com>
2024-07-20 23:24:25 -0300

    (+) Create index and show routes to get roles data

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,8 +1,18 @@
 class RolesController < ApplicationController
   def index
+    roles = Role.all
+
+    data = RolesPresenter.new(roles).serialize
+
+    render json: data
   end
 
   def show
+    role = Role.find(params[:id])
+
+    data = RolesPresenter.new(role).serialize
+
+    render json: data
   end
 
   def create
--- spec/requests/roles_spec.rb
+++ spec/requests/roles_spec.rb
@@ -1,39 +1,67 @@
 require 'rails_helper'
+require 'swagger_helper'
 
 RSpec.describe "Roles", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/roles/index"
-      expect(response).to have_http_status(:success)
+  path '/roles' do
+    get 'List roles' do
+      tags 'Roles'
+      produces 'application/json'
+
+      response '200', 'roles listed' do
+        before { create_list(:role, 1) }
+
+        schema(
+          type: :object,
+          properties: {
+            roles: {
+              type: :array,
+              items: {
+                type: :object,
+                properties: {
+                  id: { type: :string, format: :uuid },
+                  name: { type: :string },
+                  description: { type: :string }
+                },
+                required: %i[id name]
+              }
+            }
+          },
+          required: %i[roles]
+        )
+
+        run_test!
+      end
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/roles/show"
-      expect(response).to have_http_status(:success)
+  path '/roles/{id}' do
+    get 'Show role' do
+      tags 'Role'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :string, format: :uuid
+
+      response '200', 'Show role' do
+        let(:role) { create(:role) }
+        let(:id) { role.id }
+
+        schema(
+          type: :object,
+          properties: {
+            role: {
+              type: :object,
+              properties: {
+                id: { type: :string, format: :uuid },
+                name: { type: :string },
+                description: { type: :string }
+              },
+              required: %i[id name]
+            }
+          },
+          required: %i[role]
+        )
+
+        run_test!
+      end
     end
   end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/roles/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/roles/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/roles/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
