989160a7eb1b09638f7b8ea236bf3e87c55fed01
Andrew Oliveira <andrewhenriquef@gmail.com>
2024-07-20 23:07:39 -0300

    (+) Create index and show routes to get users data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    users = User.includes(:roles).all
 
-    render json: @users, include: :roles
+    data = UsersPresenter.new(users).serialize
+
+    render json: data
   end
 
   def show
-    @user = User.find(params[:id])
+    user = User.includes(:roles).find(params[:id])
 
-    render json: @user, include: :roles
+    data = UsersPresenter.new(user).serialize
+
+    render json: data
   end
 
   def create
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,39 +1,91 @@
 require 'rails_helper'
+require 'swagger_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/users/index"
-      expect(response).to have_http_status(:success)
+  path '/users' do
+    get 'List users' do
+      tags 'Users'
+      produces 'application/json'
+
+      response '200', 'users listed' do
+        before do
+          create_list(:membership, 2)
+        end
+
+        schema(
+          type: :object,
+          properties: {
+            users: {
+              type: :array,
+              items: {
+                type: :object,
+                properties: {
+                  id: { type: :string, format: :uuid },
+                  name: { type: :string },
+                  roles: {
+                    type: :array,
+                    items: {
+                      type: :object,
+                      properties: {
+                        id: { type: :string, format: :uuid },
+                        name: { type: :string },
+                        description: { type: :string }
+                      },
+                      required: %i[id name]
+                    }
+                  }
+                },
+                required: %i[id name]
+              }
+            }
+          },
+          required: %i[users]
+        )
+
+        run_test!
+      end
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
-      expect(response).to have_http_status(:success)
+  path '/users/{id}' do
+    get 'Show team' do
+      tags 'Team'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :string, format: :uuid
+
+      response '200', 'Show team' do
+        let(:membership) { create(:membership) }
+        let(:user) { membership.user }
+        let(:id) { user.id }
+
+        schema(
+          type: :object,
+          properties: {
+            user: {
+              type: :object,
+              properties: {
+                id: { type: :string, format: :uuid },
+                name: { type: :string },
+                roles: {
+                  type: :array,
+                  items: {
+                    type: :object,
+                    properties: {
+                      id: { type: :string, format: :uuid },
+                      name: { type: :string },
+                      description: { type: :string }
+                    },
+                    required: %i[id name]
+                  }
+                }
+              }
+            }
+          },
+          required: %i[user]
+        )
+
+        run_test!
+      end
     end
   end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/users/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/users/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/users/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
